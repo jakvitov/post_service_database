5159e5f26daf262982601698e5e182d228c030e5
Jakub Vítovec <jakvitov@gmail.com>
2022-07-22 20:50:53 +0200

    New create script for relations between tables

---
+++ create_scripts/ps_create_script_relations.sql
@@ -0,0 +1,15 @@
+--Foreign keys and relations names
+--Foreign key constraints names - fk_ + referenced_table_name
+begin;
+ALTER TABLE ps_post ADD user_id int;
+ALTER TABLE ps_post ADD CONSTRAINT fk_ps_user FOREIGN KEY (user_id) REFERENCES ps_user(user_id);
+
+ALTER TABLE ps_post ADD topic_id int;
+ALTER TABLE ps_post ADD CONSTRAINT fk_ps_topic FOREIGN KEY (topic_id) REFERENCES ps_topic(topic_id);
+
+ALTER TABLE ps_post_comment ADD post_id int;
+ALTER TABLE ps_post_comment ADD CONSTRAINT fk_ps_post FOREIGN KEY (post_id) REFERENCES ps_post(post_id);
+
+ALTER TABLE ps_topic ADD user_id int;
+ALTER TABLE ps_topic ADD CONSTRAINT fk_ps_user FOREIGN KEY (user_id) REFERENCES ps_user(user_id);
+commit;
